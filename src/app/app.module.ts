5324d55452a94b60073946c87a1f39cf3673a87c
andresgIES <andres.garcia@iesonline.com.co>
2021-01-22 16:06:10 -0500

    refactor: se borra codigo inicial para empezar desde cero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { UploadBetsComponent } from './upload-bets/upload-bets.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, UploadBetsComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
