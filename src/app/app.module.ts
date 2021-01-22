03578b40c5344255e19484845bab0e4eb03992f4
andresgIES <andres.garcia@iesonline.com.co>
2021-01-22 16:41:12 -0500

    fix: se añade HttpClientModulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from "./app.component";
 import { ConsultarComicComponent } from "./components/consultar-comic/consultar-comic.component";
 import { ConsultarPersonajeComponent } from "./components/consultar-personaje/consultar-personaje.component";
 import { ConsultarSeriesComponent } from "./components/consultar-series/consultar-series.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [
     AppComponent,
     ConsultarComicComponent,
